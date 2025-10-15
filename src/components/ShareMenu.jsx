570c49ab5296898692da9544530ef15d305dca32
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-10-15 09:28:28 +0000

    trashing/logs

--- src/components/ShareMenu.jsx
+++ src/components/ShareMenu.jsx
@@ -1,26 +1,39 @@
 
 'use client'
-import { Share2 } from 'lucide-react'
+import { Share2, Copy, Mail, Linkedin, Twitter, Facebook } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
+import { toast } from 'react-hot-toast'
 
 export default function ShareMenu({ blogTitle, blogUrl }) {
   const shareOptions = [
     {
-      name: 'Twitter',
-      url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(blogTitle)}&url=${encodeURIComponent(blogUrl)}`
+      name: 'Copy Link',
+      icon: <Copy size={18} />,
+      action: () => {
+        navigator.clipboard.writeText(blogUrl)
+        toast.success('Link copied to clipboard!')
+      },
+    },
+    {
+      name: 'Email',
+      icon: <Mail size={18} />,
+      url: `mailto:?subject=${encodeURIComponent(blogTitle)}&body=${encodeURIComponent(blogUrl)}`
     },
     {
       name: 'LinkedIn',
+      icon: <Linkedin size={18} />,
       url: `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(blogUrl)}`
     },
     {
-      name: 'Facebook',
-      url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(blogUrl)}`
+      name: 'Twitter',
+      icon: <Twitter size={18} />,
+      url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(blogTitle)}&url=${encodeURIComponent(blogUrl)}`
     },
     {
-      name: 'Reddit',
-      url: `https://www.reddit.com/submit?title=${encodeURIComponent(blogTitle)}&url=${encodeURIComponent(blogUrl)}`
-    }
+      name: 'Facebook',
+      icon: <Facebook size={18} />,
+      url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(blogUrl)}`
+    },
   ]
 
   return (
@@ -34,7 +47,17 @@ export default function ShareMenu({ blogTitle, blogUrl }) {
       <DropdownMenuContent>
         {shareOptions.map(option => (
           <DropdownMenuItem key={option.name} asChild>
-            <a href={option.url} target="_blank" rel="noopener noreferrer">{option.name}</a>
+            {option.url ? (
+              <a href={option.url} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+                {option.icon}
+                <span>{option.name}</span>
+              </a>
+            ) : (
+              <button onClick={option.action} className="flex items-center gap-2">
+                {option.icon}
+                <span>{option.name}</span>
+              </button>
+            )}
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
