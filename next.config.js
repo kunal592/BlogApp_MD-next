6e20b372a97a428df41d6448271b09234ed95e4c
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-10-16 10:06:47 +0000

    flush1

---
+++ next.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.pinimg.com', 'lh3.googleusercontent.com', 'images.unsplash.com'],
+  },
+};
+
+module.exports = nextConfig;
